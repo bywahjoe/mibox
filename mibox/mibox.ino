00b7817c11bc12f3ef053d2c2958bfcb0bcde44d
siwhy <wahjoe.why@gmail.com>
2023-06-27 17:39:36 +0700

    V1.2

--- mibox/mibox.ino
+++ mibox/mibox.ino
@@ -1,9 +1,72 @@
+/*
+  Firmansyah Wahyu @2023
+  CODE: https://github.com/bywahjoe/mibox
+*/
+
+#include "pinku.h"
+#define button1 digitalRead(btn1)
+#define button2 digitalRead(btn2)
+#define button3 digitalRead(btn3)
+#define button4 digitalRead(btn4)
+#define button5 digitalRead(btn5)
+#define button6 digitalRead(btn6)
+#define button7 digitalRead(btn7)
+#define button8 digitalRead(btn8)
+
+String view[9] = {"STARTING", "KUBUS", "BALOK", "PRISMA SEGITIGA", "TABUNG", "BOLA", "KERUCUT", "LIMAS SEGITIGA", "LIMAS SEGIEMPAT"};
+int setDelay = 3000;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+  pinMode(btn3, INPUT_PULLUP);
+  pinMode(btn4, INPUT_PULLUP);
+  pinMode(btn5, INPUT_PULLUP);
+  pinMode(btn6, INPUT_PULLUP);
+  pinMode(btn7, INPUT_PULLUP);
+  pinMode(btn8, INPUT_PULLUP);
 
+  delay(200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (!button1) {
+    play(1);
+    delay(setDelay);
+  }
+  if (!button2) {
+    play(2);
+    delay(setDelay);
+  }
+  if (!button3) {
+    play(3);
+    delay(setDelay);
+  }
+  if (!button4) {
+    play(4);
+    delay(setDelay);
+  }
+  if (!button5) {
+    play(5);
+    delay(setDelay);
+  }
+  if (!button6) {
+    play(6);
+    delay(setDelay);
+  }
+  if (!button7) {
+    play(7);
+    delay(setDelay);
+  }
+  if (!button8) {
+    play(8);
+    delay(setDelay);
+  }
+}
+void play(int number) {
+  Serial.print("BUTTON PRESS:"); Serial.println(number);
+  Serial.print("PLAYING     :"); Serial.println(view[number]);
 }
---
+++ mibox/pinku.h
@@ -0,0 +1,14 @@
+//Button
+#define btn1 4
+#define btn2 5
+#define btn3 6
+#define btn4 7
+#define btn5 8
+#define btn6 9
+#define btn7 10
+#define btn8 11
+
+
+//MP3
+#define myRX 2
+#define myTX 3
